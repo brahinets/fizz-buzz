4f899bf410b34135251df2bcf76b749d846c8be7
Yaroslav Brahinets <y.brahinets@gmail.com>
2022-08-06 16:14:03 +0300

    Enable JUnit testing

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+    @Test
+    void dummy() {
+        boolean itWorks = true;
+
+        assertTrue(itWorks);
+    }
+}
